80e2023cbebc9f1922912731de66ed1b37656451
Ria Carmin <riacarmin@microsoft.com>
2022-08-09 18:48:19 -0700

    Updates docs

--- docs/assets/es.string.from-code-point.8b01f754.js
+++
@@ -1,2 +0,0 @@
-import{_ as i,f as v,k as d}from"./iframe.95ef9aff.js";var f=i,s=v,g=d,m=RangeError,e=String.fromCharCode,n=String.fromCodePoint,u=s([].join),C=!!n&&n.length!=1;f({target:"String",stat:!0,arity:1,forced:C},{fromCodePoint:function(h){for(var t=[],a=arguments.length,o=0,r;a>o;){if(r=+arguments[o++],g(r,1114111)!==r)throw m(r+" is not a valid code point");t[o]=r<65536?e(r):e(((r-=65536)>>10)+55296,r%1024+56320)}return u(t,"")}});
-//# sourceMappingURL=es.string.from-code-point.8b01f754.js.map
