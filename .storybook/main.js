f71b2e0cb212623e1144c1d0f2e3911e88472f18
Ria Carmin <riacarmin@microsoft.com>
2022-08-09 19:01:09 -0700

    Updates docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,15 +5,22 @@ module.exports = {
     '../hook/**/*.stories.mdx',
     '../hooks/**/*.stories.@(js|jsx|ts|tsx)',
     '../styles/**/*.stories.mdx',
-    '../styles/**/*.stories.@(js|jsx|ts|tsx)'
+    '../styles/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        // https://github.com/storybookjs/storybook/issues/15901
+        // docs not compatible with webpack 5.
+        docs: false,
+      },
+    },
     '@storybook/addon-interactions',
   ],
   framework: '@storybook/react',
   core: {
-    builder: '@storybook/builder-vite'
+    builder: '@storybook/builder-vite',
   },
 };
