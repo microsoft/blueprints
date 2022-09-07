27c391bcd1559e19b01b0b9b58a416d3ad5574c8
Ria Carmin <riacarmin@microsoft.com>
2022-09-06 18:41:52 -0700

    Adds motion to MainNavigation component

--- components/main-navigation/src/main-navigation-sub/main-navigation-sub.tsx
+++ components/main-navigation/src/main-navigation-sub/main-navigation-sub.tsx
@@ -1,36 +1,55 @@
 import '@reach/accordion/styles.css';
 
 import { Icon } from '@arbutus/component.icon';
-import {
-  Accordion,
-  AccordionButton,
-  AccordionItem,
-  AccordionPanel,
-} from '@reach/accordion';
+import { AnimatePresence } from 'framer-motion';
+import { nanoid } from 'nanoid';
 import type { FC } from 'react';
+import { useMemo, useState } from 'react';
 import * as React from 'react';
 
 import { useMainNavigationSubStyles } from './main-navigation-sub.styles';
 import type { MainNavigationSubProps } from './main-navigation-sub.types';
+import { FadeMotion, SlideMotion } from './motion';
 
 export const MainNavigationSub: FC<MainNavigationSubProps> = ({
   className,
   children,
   title,
+  initial = 'collapsed',
 }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const handleExpandClick = () => setIsExpanded(!isExpanded);
+
+  const a11yLabelId = useMemo(() => nanoid(), []);
+  const a11yContentId = useMemo(() => nanoid(), []);
+
   const classes = useMainNavigationSubStyles();
 
   return (
-    <Accordion className={className} collapsible>
-      <AccordionItem>
-        <AccordionButton className={classes.button}>
-          <Icon iconName="chevron-right" color="tertiary" />
-          {title}
-        </AccordionButton>
-        <AccordionPanel>
-          <div className={classes.subList}>{children}</div>
-        </AccordionPanel>
-      </AccordionItem>
-    </Accordion>
+    <div className={className}>
+      <button
+        className={classes.button}
+        id={a11yLabelId}
+        aria-controls={a11yContentId}
+        aria-expanded={isExpanded}
+        onClick={handleExpandClick}
+      >
+        <Icon iconName="chevron-right" color="tertiary" />
+        {title}
+      </button>
+      <AnimatePresence initial={false}>
+        {isExpanded && (
+          <SlideMotion
+            initial={initial}
+            a11yContentId={a11yContentId}
+            a11yLabelId={a11yLabelId}
+          >
+            <FadeMotion initial={initial}>
+              <div className={classes.subList}>{children}</div>
+            </FadeMotion>
+          </SlideMotion>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
