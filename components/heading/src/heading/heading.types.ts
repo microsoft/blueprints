92bd51ef0afbf9d0fb087c30f616f70dfa98c675
Ria Carmin <riacarmin@microsoft.com>
2022-10-27 00:51:40 -0700

    Improves copy functionality in Heading

--- components/heading/src/heading/heading.types.ts
+++ components/heading/src/heading/heading.types.ts
@@ -1,30 +1,31 @@
 import type { TextProps } from '@microsoft/arbutus.text';
 
+export type OnCopyArgs = { url: string };
+
 export type HeadingUrlProps = {
   /**
    * Link to the heading section.
    */
-  headingUrl?: string;
+  headingUrl: string;
 
   /**
    * Screen-reader accessible label for the copy heading URL action.
    * @default 'Copy heading URL to clipboard.'
    */
   copyLabel?: string;
+
+  /**
+   * onCopy callback that fires after a successful copy heading URL action.
+   */
+  onCopy?: (args0: OnCopyArgs) => void;
 };
 
 export type NoHeadingUrlProps = {
-  /**
-   * Link to the heading section.
-   */
-  headingUrl?: never;
-
-  /**
-   * Screen-reader accessible label for the copy heading URL action.
-   * @default 'Copy heading URL to clipboard.'
-   */
+  headingUrl: undefined;
   copyLabel?: never;
+  onCopy?: (args0: OnCopyArgs) => void;
 };
+
 export type HeadingProps = {
   /**
    * Consumer provided class.
@@ -36,5 +37,10 @@ export type HeadingProps = {
    * @default 'h1'
    */
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+  /**
+   * Heading id.
+   */
+  id?: string;
 } & Pick<TextProps, 'color' | 'variant'> &
   (HeadingUrlProps | NoHeadingUrlProps);
