0c23a5b4b5428a9348b1868dac028688c128a7ac
AkimaLunar <akimalunar@gmail.com>
2023-02-06 18:03:45 -0800

    Adds code variant

--- components/text/src/text/text.types.ts
+++ components/text/src/text/text.types.ts
@@ -1,7 +1,7 @@
 import type { TextProps as FluentTextProps } from '@fluentui/react-text';
 import type { ReactNode } from 'react';
 
-export type StyleVariant = 'jumbo' | 'title' | 'headline' | 'subtitle' | 'leading' | 'paragraph' | 'caption' | 'description';
+export type StyleVariant = 'jumbo' | 'title' | 'headline' | 'subtitle' | 'leading' | 'paragraph' | 'caption' | 'description' | 'code';
 
 export type ColorVariant = 'accent' | 'action' | 'primary' | 'secondary' | 'tertiary' | 'quaternary' | 'negative' | 'positive' | 'warning' | 'danger' | 'info' | 'background';
 
