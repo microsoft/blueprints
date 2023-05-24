c250124189ca5f568ee8912cfac9307084858286
AkimaLunar <akimalunar@gmail.com>
2023-05-23 17:24:51 -0700

    Package upgrades

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,46 +1,34 @@
 const webpack = require('webpack');
-
 module.exports = {
   stories: [
-    '../components/**/*.stories.mdx',
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
-    '../hooks/**/*.stories.mdx',
     '../hooks/**/*.stories.@(js|jsx|ts|tsx)',
-    '../utilities/**/*.stories.mdx',
     '../utilities/**/*.stories.@(js|jsx|ts|tsx)',
-    '../styles/**/*.stories.mdx',
-    '../styles/**/*.stories.@(js|jsx|ts|tsx)',
+    '../styles/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  addons: [
-    '@storybook/addon-links',
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        // https://github.com/storybookjs/storybook/issues/15901
-        // docs not compatible with webpack 5.
-        docs: false,
-      },
-    },
-    '@storybook/addon-interactions',
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
-    // Bug in @storybook/builder-vite: https://github.com/storybookjs/storybook/issues/18920
-    // builder: '@storybook/builder-vite',
+  addons: ['@storybook/addon-links', {
+    name: '@storybook/addon-essentials',
+    options: {
+      // https://github.com/storybookjs/storybook/issues/15901
+      // docs not compatible with webpack 5.
+      docs: false
+    }
+  }, '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async (config, {
+    configType
+  }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
 
     // Make whatever fine-grained changes you need
-    config.plugins = [
-      ...config.plugins,
-      new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
-      }),
-    ]
+    config.plugins = [...config.plugins, new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer']
+    })];
     config.resolve.fallback = {
       ...config.resolve.fallback,
       assert: require.resolve('assert/'),
@@ -48,10 +36,13 @@ module.exports = {
       fs: false,
       module: false,
       os: require.resolve('os-browserify/browser'),
-      process: require.resolve('process/browser'),
+      process: require.resolve('process/browser')
     };
 
     // Return the altered config
     return config;
   },
+  docs: {
+    autodocs: true
+  }
 };
