6b1124f13c2d5c473bd8c3dec04147bac769b81a
Ria Carmin <akimalunar@gmail.com>
2023-09-05 15:38:00 -0700

    Adds annotation to generateTokensMap

--- hooks/use-css-vars/src/generate-tokens-map.ts
+++ hooks/use-css-vars/src/generate-tokens-map.ts
@@ -16,6 +16,47 @@ const isEmptyObject = (obj: Value) => {
   }
 };
 
+/**
+ * Generates a map of CSS variables from a theme object. Use this helper to generate a map of CSS variables that you can
+ * use in your style files.
+ * @param theme Theme object you want to map to CSS variables.
+ * @param prefix Optional prefix to add to the CSS variable name (e.g. `--my-prefix--color-button-primary`).
+ * @returns A map of CSS variables, where the key is the same as in your theme object, but the value is a CSS variable.
+ *
+ * @example
+ *
+ * ```tsx
+ * import { generateTokensMap } from '@microsoft/arbutus.use-css-vars';
+ *
+ * const lightTheme: Theme = {
+ *   color: {
+ *     button: {
+ *       primary: '#fff',
+ *       secondary: '#000',
+ *     },
+ *   },
+ * };
+ *
+ * // NOTE: If you are using TypeScript, create a `Theme` type for you custom themes to get type safety.
+ *
+ * const myCustomTokens = generateTokensMap<Theme>(lightTheme, 'my-prefix');
+ * // Output:
+ * // {
+ * //  color: {
+ * //    button: {
+ * //      primary: 'var(--my-prefix--color-button-primary)',
+ * //      secondary: 'var(--my-prefix--color-button-secondary)',
+ * //    },
+ * //  },
+ * //};
+ *
+ * const myStyles = {
+ *  button: {
+ *   color: myCustomTokens.color.button.primary,
+ * },
+ *
+ * ```
+ */
 export function generateTokensMap<T>(theme: Record<string, Value>, prefix?: string): T {
   const traverseObject = (obj: Theme, pre: string) => {
     const newObj: Record<string, any> = {};
