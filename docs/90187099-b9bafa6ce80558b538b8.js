6fda2d2ac8cc9c2749f9dd06cb8adcd08fda0e33
Ria Carmin <akimalunar@gmail.com>
2023-11-28 16:23:40 -0800

    Updates docs

---
+++ docs/90187099-b9bafa6ce80558b538b8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkarbutus_docs=self.webpackChunkarbutus_docs||[]).push([[556],{404:function(u,c,n){n.d(c,{X9O:function(){return e}});var s=n(1282);const e=(0,s.k)("ChevronDownRegular","1em",["M15.85 7.65c.2.2.2.5 0 .7l-5.46 5.49a.55.55 0 0 1-.78 0L4.15 8.35a.5.5 0 1 1 .7-.7L10 12.8l5.15-5.16c.2-.2.5-.2.7 0Z"])}}]);
+//# sourceMappingURL=90187099-b9bafa6ce80558b538b8.js.map
