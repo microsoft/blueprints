f8c6aec8483a8c2311130450a1a1ef9afcf98d18
Ria Carmin <akimalunar@gmail.com>
2024-03-05 16:14:13 -0800

    Runs lint

--- utilities/docs/src/code-examples/text.example.tsx
+++ utilities/docs/src/code-examples/text.example.tsx
@@ -45,11 +45,11 @@ const ExampleComponent = () => {
       <Text block as="h3" variant="subtitle" className={space.mb2}>
         Subheading
       </Text>
-      <Text block as="p"  variant="leading">
+      <Text block as="p" variant="leading">
         Leading Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate modi
-        eveniet dolorum <Text variant='code'>code</Text> assumenda similique a voluptas voluptatum ducimus
-        temporibus. Culpa animi labore molestiae nesciunt suscipit, architecto optio sit
-        iusto.
+        eveniet dolorum <Text variant="code">code</Text> assumenda similique a voluptas
+        voluptatum ducimus temporibus. Culpa animi labore molestiae nesciunt suscipit,
+        architecto optio sit iusto.
       </Text>
 
       <Divider className={space.my8} />
@@ -59,20 +59,30 @@ const ExampleComponent = () => {
       </Text>
       <Text block as="p" variant="paragraph" className={space.mb8}>
         Paragraph Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate
-        modi eveniet dolorum officiis <Text variant='code'>code</Text> similique a voluptas voluptatum ducimus
-        temporibus. Culpa animi labore molestiae nesciunt suscipit, architecto optio sit
-        iusto.
+        modi eveniet dolorum officiis <Text variant="code">code</Text> similique a
+        voluptas voluptatum ducimus temporibus. Culpa animi labore molestiae nesciunt
+        suscipit, architecto optio sit iusto.
       </Text>
       <div className={classes.grid}>
         <article>
           <div className={classes.image}>&nbsp;</div>
-          <Text block variant='caption' className={space.mb2}>Caption</Text>
-          <Text block variant='description'>Description. Culpa animi labore molestiae nesciunt suscipit. Similique a voluptas voluptatum ducimus temporibus.</Text>
+          <Text block variant="caption" className={space.mb2}>
+            Caption
+          </Text>
+          <Text block variant="description">
+            Description. Culpa animi labore molestiae nesciunt suscipit. Similique a
+            voluptas voluptatum ducimus temporibus.
+          </Text>
         </article>
         <article>
           <div className={classes.image}>&nbsp;</div>
-          <Text block variant='caption' className={space.my2}>Caption</Text>
-          <Text block variant='description'>Description. Culpa animi labore molestiae nesciunt suscipit. Sit amet consectetur adipisicing elit. Cupiditate modi eveniet dolorum.</Text>
+          <Text block variant="caption" className={space.my2}>
+            Caption
+          </Text>
+          <Text block variant="description">
+            Description. Culpa animi labore molestiae nesciunt suscipit. Sit amet
+            consectetur adipisicing elit. Cupiditate modi eveniet dolorum.
+          </Text>
         </article>
       </div>
     </main>
