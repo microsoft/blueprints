cda7d2a757d43504db9ca25ab4448dcbc08aab81
Ria Carmin <akimalunar@gmail.com>
2024-03-18 12:04:30 -0700

    Creates prop docs for all lists

--- components/mark-list/__dev__/mark-list.stories.tsx
+++ components/mark-list/__dev__/mark-list.stories.tsx
@@ -1,17 +1,14 @@
 import { makeStyles } from '@griffel/react';
 import { colorClasses, paths } from '@microsoft/arbutus.icon';
 import { Text } from '@microsoft/arbutus.text';
-import type { Meta, StoryFn } from '@storybook/react';
-import type { FunctionComponent } from 'react';
+import type { Meta, StoryFn, StoryObj } from '@storybook/react';
 import React from 'react';
 
 import { Centered } from '../../../.storybook/decorators';
-import type { MarkListProps } from '../src/index';
 import { MarkList, MarkListItem } from '../src/index';
 
 export default {
   title: 'Lists/MarkList',
-  component: MarkList,
   argTypes: {
     iconName: {
       options: Object.keys(paths),
@@ -38,51 +35,6 @@ const useTileGridStyles = makeStyles({
     },
   },
 });
-
-const Template: StoryFn<typeof MarkList> = (args) => (
-  <MarkList {...args}>
-    <MarkListItem>
-      <Text as="span" variant="caption">
-        Natural on every platform.{' '}
-      </Text>
-      <Text as="span" color="secondary">
-        You want to know what to do. Your experiences should adapt to the device you’re on
-        and should build off the familiar — designing for what you already understand. You
-        want to know what to do. Your experiences should adapt to the device you’re on and
-        should build off the familiar — designing for what you already understand.
-      </Text>
-    </MarkListItem>
-    <MarkListItem>
-      <Text as="span" variant="caption">
-        One for all, all for one. You want to be included.{' '}
-      </Text>
-      <Text as="span" color="secondary">
-        Your experiences should consider, learn, and reflect a range of perspectives and
-        abilities for the benefit of all.
-      </Text>
-    </MarkListItem>
-    <MarkListItem>
-      <Text as="span" variant="caption">
-        Built for focus. You want to stay in the flow.{' '}
-      </Text>
-
-      <Text as="span" color="secondary">
-        Your experiences should inspire action, drawing you forward, simply and
-        seamlessly.
-      </Text>
-    </MarkListItem>
-    <MarkListItem>
-      <Text as="span" variant="caption">
-        Unmistakably Microsoft.{' '}
-      </Text>
-
-      <Text as="span" color="secondary">
-        You want to recognize what you’re looking for. Your experiences should feel like
-        one Microsoft. One moment, one product, one experience at a time.
-      </Text>
-    </MarkListItem>
-  </MarkList>
-);
 
 const TemplateWithIcon: StoryFn<typeof MarkList> = (args) => {
   const classes = useTileGridStyles();
@@ -113,17 +65,66 @@ const TemplateWithIcon: StoryFn<typeof MarkList> = (args) => {
   );
 };
 
-export const Simple = Template.bind({}) as StoryFn<FunctionComponent<MarkListProps>>;
-Simple.args = {
-  iconName: undefined,
-  iconColor: undefined,
-};
+// export const Simple = Template.bind({}) as StoryFn<FunctionComponent<MarkListProps>>;
+// Simple.args = {
+//   iconName: undefined,
+//   iconColor: undefined,
+// };
 
-export const WithIcon = TemplateWithIcon.bind({}) as StoryFn<
-  FunctionComponent<MarkListProps>
->;
+// export const WithIcon = TemplateWithIcon.bind({}) as StoryFn<
+//   FunctionComponent<MarkListProps>
+// >;
 
-WithIcon.args = {
-  iconName: 'check',
-  iconColor: 'positive',
+// WithIcon.args = {
+//   iconName: 'check',
+//   iconColor: 'positive',
+// };
+
+export const Base: StoryObj = {
+  render: (args) => (
+    <p>Hello {JSON.stringify(args)}</p>
+    // <MarkList {...args}>
+    //   <MarkListItem>
+    //     <Text as="span" variant="caption">
+    //       Natural on every platform.{' '}
+    //     </Text>
+    //     <Text as="span" color="secondary">
+    //       You want to know what to do. Your experiences should adapt to the device you’re
+    //       on and should build off the familiar — designing for what you already
+    //       understand. You want to know what to do. Your experiences should adapt to the
+    //       device you’re on and should build off the familiar — designing for what you
+    //       already understand.
+    //     </Text>
+    //   </MarkListItem>
+    //   <MarkListItem>
+    //     <Text as="span" variant="caption">
+    //       One for all, all for one. You want to be included.{' '}
+    //     </Text>
+    //     <Text as="span" color="secondary">
+    //       Your experiences should consider, learn, and reflect a range of perspectives and
+    //       abilities for the benefit of all.
+    //     </Text>
+    //   </MarkListItem>
+    //   <MarkListItem>
+    //     <Text as="span" variant="caption">
+    //       Built for focus. You want to stay in the flow.{' '}
+    //     </Text>
+
+    //     <Text as="span" color="secondary">
+    //       Your experiences should inspire action, drawing you forward, simply and
+    //       seamlessly.
+    //     </Text>
+    //   </MarkListItem>
+    //   <MarkListItem>
+    //     <Text as="span" variant="caption">
+    //       Unmistakably Microsoft.{' '}
+    //     </Text>
+
+    //     <Text as="span" color="secondary">
+    //       You want to recognize what you’re looking for. Your experiences should feel like
+    //       one Microsoft. One moment, one product, one experience at a time.
+    //     </Text>
+    //   </MarkListItem>
+    // </MarkList>
+  ),
 };
